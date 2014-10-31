7a229e03d39d872b301fdd3e8b320fb4c8cc4443
Fabrício Ronchi <fabricioronchii@gmail.com>
2014-10-31 08:01:44 -0200

    Ups

--- Projeto/Projeto.ino
+++ Projeto/Projeto.ino
@@ -1,14 +1,19 @@
-int const buttonInterval = 10;
+int const buttonInterval1 = 10;
+int const buttonInterval2 = 9;
+int const buttonInterval3 = 8;
 int const buttonShow = 9;
-int intervals[] = { 15000, 30000, 45000, 60000, 90000, 120000 };
-int interval = 0;
+int display1 = 0;
+int display2 = 0;
+int display3 = 0;
 int counter = 1000;
 int timer = 5000;
 boolean show = false;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(buttonInterval, INPUT);
+  pinMode(buttonInterval1, INPUT);
+  pinMode(buttonInterval2, INPUT);
+  pinMode(buttonInterval3, INPUT);
   pinMode(buttonShow, INPUT);
 }
 
@@ -18,8 +23,20 @@ void loop() {
     setTimer(getTimer() - 1000); 
     delay(1000);
   }else{
-    if(digitalRead(buttonInterval) == 1){
-      setInterval(getInterval() + 1);
+    if(digitalRead(buttonInterval1) == 1){
+      setInterval1(getInterval1() + 1);
+      showDisplay();
+      delay(800);
+    }
+    if(digitalRead(buttonInterval2) == 1){
+      setInterval2(getInterval2() + 1);
+      showDisplay();
+      delay(800);
+    }
+    if(digitalRead(buttonInterval3) == 1){
+      setInterval1(getInterval3() + 1);
+      showDisplay();
+      delay(800);
     }
     if(digitalRead(buttonShow) == 1){    
       getCounter(getInterval());
@@ -30,28 +47,61 @@ void loop() {
 
 void finish(){
   setShow(false);
-  interval = 0;
+  display1 = 0;
+  display2 = 0;
+  display3 = 0;
   timer = 5000;
 }
 
 void actions(){
 }
 
-void setInterval(int new_interval){
-  if(new_interval <= 5){
-    interval = new_interval;
+void setInterval1(int new_interval){
+  if(new_interval <= 9){
+    display1 = new_interval;
   }else{
-    interval = 0;
+    display1 = 0;
   }
 }
 
-int getInterval(){
-  return interval;
+void setInterval2(int new_interval){
+  if(new_interval <= 9){
+    display2 = new_interval;
+  }else{
+    display2 = 0;
+  }
 }
 
-void getCounter(int new_counter){
-  setTimer(intervals[new_counter]);
-  setShow(true); 
+void setInterval3(int new_interval){
+  if(new_interval <= 9){
+    display3 = new_interval;
+  }else{
+    display3 = 0;
+  }
+}
+
+int getInterval1(){
+  return display1;
+}
+
+int getInterval2(){
+  return display2;
+}
+
+int getInterval3(){
+  return display3;
+}
+
+int getInterval(){
+  return (getInterval1() * 100) + (getInterval2() * 10) + getInterval3();
+}
+
+void getCounter(int new_interval){
+  setTimer(new_interval);
+}
+
+void showDisplay(){
+  Serial.println((getInterval1() * 100) + (getInterval2() * 10) + getInterval3());
 }
 
 void setShow(boolean new_show){
@@ -65,6 +115,7 @@ int getShow(){
 void setTimer(int new_timer){
   if(new_timer > 0){
     timer = new_timer;
+    setShow(true); 
   }else{
     finish();
   }
