1709bee1c642c51918b9e0af2404eb142eef859b
Fabricio Ronchi <fabricioronchii@gmail.com>
2014-10-31 23:17:26 -0200

    Feito

--- Projeto/Projeto.ino
+++ Projeto/Projeto.ino
@@ -1,135 +1,118 @@
-#include "SevSeg.h"
+#include "SevSeg.h" 
+   
+SevSeg sevseg;  
+int start = A1;
+int centena = A2;
+int dezena = A3;
+int unidade = A4;
+int rele = A5;
+int buzzer = 13;
+int centenaNumber = 0;
+int dezenaNumber = 0;
+int unidadeNumber = 0;
+int regressivo = 000;
+int atual = 0;
+int anterior = 0;
+boolean comecar = false;
 
-SevSeg sevseg;
-
-int const buttonInterval1 = 10;
-int const buttonInterval2 = 9;
-int const buttonInterval3 = 8;
-int const buttonShow = 9;
-int display1 = 0;
-int display2 = 0;
-int display3 = 0;
-int timer = 0;
-boolean show = false;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(buttonInterval1, INPUT);
-  pinMode(buttonInterval2, INPUT);
-  pinMode(buttonInterval3, INPUT);
-  pinMode(buttonShow, INPUT);
-  //sevseg.Begin(1,13,10,11,12,2,3,4,5,6,7,8,9);  
-  //sevseg.Brightness(50);
-}
-
-void loop() {
-  if(getShow() == true){
-    if(setTimer(getTimer() - 1) == true){
-      actions();
-      showDisplay();
-      delay(1000);
-    }
-  }else{
-    if(digitalRead(buttonInterval1) == 1){
-      setInterval1(getInterval1() + 1);
-      showDisplay();
-      delay(800);
-    }
-    if(digitalRead(buttonInterval2) == 1){
-      setInterval2(getInterval2() + 1);
-      showDisplay();
-      delay(800);
-    }
-    if(digitalRead(buttonInterval3) == 1){
-      setInterval1(getInterval3() + 1);
-      showDisplay();
-      delay(800);
-    }
-    if(digitalRead(buttonShow) == 1){    
-      setTimer(getCounter());
-      delay(1000);
-    }
-    delay(100);
+void setup() 
+{  
+  sevseg.Begin(false, false,10,11,12,2,3,4,5,6,7,8,9);  
+  sevseg.Brightness(20);
+  pinMode(start, INPUT);
+  pinMode(centena, INPUT); 
+  pinMode(dezena, INPUT);
+  pinMode(unidade, INPUT);
+  pinMode(rele, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(rele, LOW);
+  digitalWrite(buzzer, LOW);
+  Serial.begin(9600);  
+}  
+   
+void loop() 
+{
+  atual = ((int) millis() / 1000);  
+  if(digitalRead(start) == 1){
+    if(comecar == false){
+      comecar = true;
+    }else{
+      comecar = false;      
+    }    
+    delay(200);
   }
-}
-
-void finish(){
-  setShow(false);
-  display1 = 0;
-  display2 = 0;
-  display3 = 0;
-  timer = 0;
-  showDisplay();
-}
-
-void actions(){
-}
-
-int getCounter(){
-  return ((getInterval1() * 100) + (getInterval2() * 10) + getInterval3());
-}
-
-void setInterval1(int new_interval){
-  if(new_interval <= 9){
-    display1 = new_interval;
-  }else{
-    display1 = 0;
-  } 
-}
-
-void setInterval2(int new_interval){
-  if(new_interval <= 9){
-    display2 = new_interval;
-  }else{
-    display2 = 0;
+  if(digitalRead(centena) == 1){
+    if(centenaNumber < 9){
+      centenaNumber = centenaNumber++;
+    }else{
+      centenaNumber = 0;
+    }    
+    regressivo = (centenaNumber * 100) + (dezenaNumber * 10) + unidadeNumber;
+    delay(200);
   }
+  if(digitalRead(dezena) == 1){
+    if(dezenaNumber < 9){
+      dezenaNumber = dezenaNumber++;
+    }else{
+      dezenaNumber = 0;
+    }    
+    regressivo = (centenaNumber * 100) + (dezenaNumber * 10) + unidadeNumber;
+    delay(200);
+  }
+  if(digitalRead(unidade) == 1){
+    if(unidadeNumber < 9){
+      unidadeNumber = unidadeNumber++;
+    }else{
+      unidadeNumber = 0;
+    }    
+    regressivo = (centenaNumber * 100) + (dezenaNumber * 10) + unidadeNumber;
+    delay(200);
+  }
+  if(comecar == true){
+    if(atual != anterior){
+      anterior = atual;
+      diminuirTempo();
+    }else{
+      digitalWrite(buzzer, LOW);
+    }
+  }
+  sevseg.PrintOutput(); 
+  sevseg.NewNum(regressivo,(byte) 2);
 }
 
-void setInterval3(int new_interval){
-  if(new_interval <= 9){
-    display3 = new_interval;
+void diminuirTempo(){
+  if(unidadeNumber > 0){
+    unidadeNumber = unidadeNumber--;
+    regressivo = (centenaNumber * 100) + (dezenaNumber * 10) + unidadeNumber; 
+    acoes(regressivo);
   }else{
-    display3 = 0;
+    if(dezenaNumber > 0){
+      dezenaNumber = dezenaNumber--;
+      unidadeNumber = 9;
+      regressivo = (centenaNumber * 100) + (dezenaNumber * 10) + unidadeNumber; 
+      acoes(regressivo);
+    }else{
+      if(centenaNumber > 0){
+        unidadeNumber = 9;
+        dezenaNumber = 9;
+        centenaNumber = centenaNumber--;
+        regressivo = (centenaNumber * 100) + (dezenaNumber * 10) + unidadeNumber; 
+        acoes(regressivo);
+      }else{
+        digitalWrite(rele, HIGH);
+        digitalWrite(buzzer, HIGH);
+        delay(500);
+        comecar = false;
+        digitalWrite(rele, LOW);
+        digitalWrite(buzzer, LOW);
+      }
+    }
+  }   
+}
+
+void acoes(int tempo){
+  Serial.println(tempo);
+  if(tempo < 6 && tempo >= 0){
+    digitalWrite(buzzer, HIGH);
   }
-}
-
-int getInterval1(){
-  return display1;
-}
-
-int getInterval2(){
-  return display2;
-}
-
-int getInterval3(){
-  return display3;
-}
-
-void showDisplay(){
-  //sevseg.PrintOutput();  
-  //sevseg.NewNum(getCounter(),(byte) 2);  
-  Serial.println(getCounter());
-}
-
-void setShow(boolean new_show){
-  show = new_show;
-}
-
-int getShow(){
-  return show;
-}
-
-boolean setTimer(int new_timer){
-  if(new_timer > 0){
-    timer = new_timer;
-    setShow(true);
-    return true;
-  }else{    
-    finish();
-    return false;
-  }
-}
-
-int getTimer(){
-  return timer;
 }
